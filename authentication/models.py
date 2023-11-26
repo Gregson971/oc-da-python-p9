76a1a7ab17cd294c8588d453b704f1595fdd2df1
Grégory Lama <gregory.lama@gmail.com>
2023-11-27 00:04:59 +0100

    feat(litrevu): create a personalized User model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    '''
+    User model for authentication.
+    It is an extension of the AbstractUser model.
+    It is used to store the user's profile photo and role.
+    '''
+
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Créateur'),
+        (SUBSCRIBER, 'Abonné'),
+    )
+    profile_photo = models.ImageField(verbose_name='Photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
